410d371a3b9860bbcf46ccb08f1daf46922b47fd
Orange <whereorange@gmail.com>
2017-09-27 19:03:14 +0700

    Waves of asteroids, fix asteroid destroing #lesson2_5

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Wait{
+	public float spawn, start, wave;
+}
+
 public class GameController : MonoBehaviour {
 	public GameObject hazard;
 	public Vector3 spawnValues;
+	public int hazardCount;
+
+	public Wait wait;
 
 	void Start () {
-		spawnWaves();	
+		StartCoroutine (spawnWaves());	
 	}
 	
-	void spawnWaves() {
-		Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate(hazard, spawnPosition, spawnRotation);
+	IEnumerator spawnWaves() {
+		yield return new WaitForSeconds (wait.start);
+		while(true){
+			for (int i =0; i < hazardCount; i++){
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate(hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (wait.spawn);
+			}
+			yield return new WaitForSeconds (wait.wave);
+		}
 	}
 }
